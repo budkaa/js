13bb91e9c71e400f78fef919420172ccdc43a0bd
budkaa <127941544+budkaa@users.noreply.github.com>
2023-04-26 11:03:03 +0000

    yey

---
+++ doc.js
@@ -0,0 +1,5 @@
+function Print(){
+    document.getElementById("name").innerHTML = document.getElementById("uname").value;
+    document.getElementById("text").innerHTML = document.getElementById("utext").value;
+    clear();
+}
